linaro-swg/gen_rootfs

commit 562ba7fb0fc4cc87f797b1951d04ac9c632dc4ec

hikey: use ${CROSS_COMPILE} if set
Similar to 1c81ed5e56fd ("vexpress: use ${CROSS_COMPILE} if set") for
HiKey. 32-bit and 64-bit mode are supported, depending on the cross-
compiler selected by ${CROSS_COMPILE}.
Tested with the following toolchain packages:
  gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
  gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux.tar.xz

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

generate-cpio-rootfs.sh
@@ -88,14 +88,32 @@ case $1 in
 
     "hikey")
         echo "Building HiKey AArch64 root filesystem"
-        if [ ! -n "${CC_DIR}" ]; then
-            echo "CC_DIR must be set as environment variable before calling this script"
+        if [ -z "${CROSS_COMPILE}${CC_DIR}" ]; then
+            echo "CROSS_COMPILE or CC_DIR must be set as environment variable before calling this script"
             exit 1
         fi
-        export ARCH=arm64
 
-        CC_PREFIX=aarch64-linux-gnu
-        LIBCBASE=${CC_DIR}/${CC_PREFIX}/libc
+        if [ "${CROSS_COMPILE}" ] ; then
+            CC_PREFIX=`${CROSS_COMPILE}gcc -print-multiarch`
+            case "${CC_PREFIX}" in
+                aarch64-linux-gnu)
+                    LIBCBASE=$(dirname $(${CROSS_COMPILE}gcc -print-file-name=ld-linux-aarch64.so.1))/..
+                    export ARCH=arm64
+                    ;;
+                arm-linux-gnueabihf)
+                    LIBCBASE=$(dirname $(${CROSS_COMPILE}gcc -print-file-name=ld-linux-armhf.so.3))/..
+                    export ARCH=arm
+                    ;;
+                *)
+                    echo "Unsupported arch"
+	            exit 1
+                    ;;
+            esac
+        else
+            LIBCBASE=${CC_DIR}/${CC_PREFIX}/libc
+            CC_PREFIX=aarch64-linux-gnu
+            export ARCH=arm64
+        fi
 
         cp etc/inittab-vexpress etc/inittab
         echo "HiKey" > etc/hostname

commit 8f4453c8918f7297d9c2b938ce6cc1213fc17739

hikey: set CC_PREFIX before using it
This fixes a bug introduced by commit 562ba7fb0fc4 ("hikey: use
${CROSS_COMPILE} if set") when CROSS_COMPILE is *not* set.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

generate-cpio-rootfs.sh
@@ -110,8 +110,8 @@ case $1 in
                     ;;
             esac
         else
-            LIBCBASE=${CC_DIR}/${CC_PREFIX}/libc
             CC_PREFIX=aarch64-linux-gnu
+            LIBCBASE=${CC_DIR}/${CC_PREFIX}/libc
             export ARCH=arm64
         fi
 