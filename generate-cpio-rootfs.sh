032dbf7276fc20e5345085113f241b91251ab1c3
Jens Wiklander <jens.wiklander@linaro.org>
2016-10-06 10:40:03 +0200

    Exclude unused large libs from rootfs
    Excludes currently unused large libs from the generated rootfs. This
    makes the rootfs small enough to be used without changes to the flash
    layout on Juno.
    
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (Juno)
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- generate-cpio-rootfs.sh
+++ generate-cpio-rootfs.sh
@@ -195,7 +195,10 @@ for file in ${SBINFILES} ; do
     echo "file /sbin/${BASE} $file 755 0 0" >> filelist-tmp.txt
 done;
 
-LIBFILES=`find ${STAGEDIR}/lib -maxdepth 1 -type f -a -not -name "*.a"`
+LIBFILES=`find ${STAGEDIR}/lib -maxdepth 1 -type f -a -not -name "*.a" \
+	-a -not -name "libstdc++*" -a -not -name "libasan*" \
+	-a -not -name "libubsan*" -a -not -name "libgfortran*" \
+	-a -not -name "libnss_nis*"`
 for file in ${LIBFILES} ; do
     BASE=`basename $file`
     echo "file /lib/${BASE} $file 755 0 0" >> filelist-tmp.txt
